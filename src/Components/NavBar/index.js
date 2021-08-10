2cb822b957d5659000fef58b9a782249150d8c3e
pasqualipedro <pasqualipedro@gmail.com>
2021-08-10 18:47:37 +0100

    Navbar and dropdown menu start

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const PartyMakerLogo = styled.h1``;
+
+
+
+function NavBar() {
+    return(
+        <div>
+            <h1>
+                PARTYMAKER
+            </h1>
+            <button>Alo</button>
+        </div>
+    )
+}
+
+export default NavBar;
