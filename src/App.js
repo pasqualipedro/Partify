b30cba5641838056b8366384065535eeb87c0607
pasqualipedro <pasqualipedro@gmail.com>
2021-08-11 21:02:13 +0100

    resolving merge issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
+import CardFeature from './Components/CardFeature'
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      
+      <NavBar />
+      <CardFeature />
     </div>
   );
 }
