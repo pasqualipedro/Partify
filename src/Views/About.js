a1bd5bee685ca6252d5f8e3de6158fa675e6cd29
LuanBarreto <luaanr@hotmail.com>
2021-08-27 22:02:56 -0300

    alterações pagina about

--- src/Views/About.js
+++ src/Views/About.js
@@ -17,11 +17,37 @@ const Wrapper = styled.div`
 function BestRankings() {
     return (
         <Wrapper>
-            <h1>This is Best Rankings</h1>
             <div>
-                <InfoTextBox />
-                <InfoTextBox />
-                <InfoTextBox />
+                <div className="infoTextBox">
+                        <h1>Why?</h1>
+                    <div>
+                        <p>
+                        . because music is fun and can bring other sensations than just information</br>
+                        .  because is fun to share music</br>
+                        . vaguely we could say the app return song´s according your fellings</br>
+                        </p>
+                    </div>
+                </div>
+                <div className="infoTextBox">
+                        <h1>How?</h1>
+                    <div>
+                        <p>
+                        . using the Spotify API + spotify-web-api-node.<br/>
+                        . using other nodes for improve dynamics and interest in UX - eg: making dynamic carousels.<br/>
+                        . with the use of React Components.<br/>
+                        . with the use of Styled-Components.<br/>
+                        . deployed with Herouku<br/>
+                        </p>
+                    </div>
+                </div>
+                <div className="infoTextBox">
+                        <h1>What?</h1>
+                    <div>
+                        <p>
+                        . An app/website to manipulate Spotify API data<br/>
+                        </p>
+                    </div>
+                </div>
             </div>
             <div className="authorInfoCards">
                 <div className="authorCard">
@@ -29,7 +55,11 @@ function BestRankings() {
                     <img src="Img/infos.png" alt="about"/>
                     <p>
                         <span class="twistFont">Twist</span>
-                        Pong is exactly and purely what it first stated: a classic game, with a - humble - twist of my own. Originaly was planned more features, but for a matter of MVP and cronogram, some had to be cut out. I plan to add those as I gain more experience and have more time. I hope you like it ;)<br/>
+                        . Pedro Adri Pasquali, Brazillian,31<br/>
+                        . Architect and urbanist with +10 years exp<br/>
+                        . post-grad in Applied Computing to Arch Urb & Design<br/>
+                        . work for IKEA Portugal, Lisbon<br/>
+                        . I have a dog named Lola, and she is adorable<br/>
                         <span class="smallLetters">Important to say here that it wasn´t intended by any way to copy any other games out there. <span class="twistFont">Twist</span> Pong was made only and purely for recreational/educational, non-commercial use</span>
                     </p>
                 </div>
@@ -38,7 +68,11 @@ function BestRankings() {
                     <img src="Img/infos.png" alt="about"/>
                     <p>
                         <span class="twistFont">Twist</span>
-                        Pong is exactly and purely what it first stated: a classic game, with a - humble - twist of my own. Originaly was planned more features, but for a matter of MVP and cronogram, some had to be cut out. I plan to add those as I gain more experience and have more time. I hope you like it ;)<br/>
+                        . Luan Barreto, Brazilian, 26<br/>
+                        . IT Management Student<br/>
+                        . Born in Rio de Janeiro, but currently living in SP<br/>
+                        . I love soccer, and doing weight training<br/>
+                        . I currently work at Leroy Merlin Morumbi, but i’ve also been to the store in Niterói<br/>
                         <span class="smallLetters">Important to say here that it wasn´t intended by any way to copy any other games out there. <span class="twistFont">Twist</span> Pong was made only and purely for recreational/educational, non-commercial use</span>
                     </p>
                 </div>
