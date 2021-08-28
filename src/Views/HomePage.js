70b8b3a5e82515ebbf80b319a3830857a3abe6a1
pasqualipedro <pasqualipedro@gmail.com>
2021-08-28 14:01:36 +0100

    token

--- src/Views/HomePage.js
+++ src/Views/HomePage.js
@@ -34,7 +34,7 @@ const PartOne = styled.section`
 class HomePage extends Component {
 
     state = {
-        moods: [`sad`, `happy`, `furious`, `frustrated`, `euphoric`, `wanna dance!`, `in love`, `drunk...`, `energetic`, `anxious`, `lazy`, `heartbreak`, `courageous`, `unstoppable`, `trash`, `as a queen`, `as a king`, `Rich`, `High`, `poor`, `cold`, `hot`, `free hugs`, `hungry`, `adventurous`, `zzz`, `sexy`, `strong`],
+        moods: [`sad`, `happy`, `furious`, `frustrated`, `euphoric`, `wanna dance!`, `in love`, `drunk...`, `energetic`, `anxious`, `lazy`, `heartbreak`, `courageous`, `unstoppable`, `trash`, `as a queen`, `as a king`, `rich`, `high`, `poor`, `cold`, `hot`, `free hugs`, `hungry`, `adventurous`, `zzz`, `sexy`, `strong`],
         playlistInfo: [],
         playlistImg: [],
         openPlaylist: false,
